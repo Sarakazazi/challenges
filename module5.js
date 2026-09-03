793826cc9229f6418669c923534cf77a5140bae8
sara kazazi <sarakazazi0@gmail.com>
2026-09-03 17:17:22 +0100

    first commit

---
+++ module5.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+const num = parseFloat(prompt("entrer un nombre :"));
+
+for (let i = 1; i <= 2*i ; i++) {
+let row = ""
+for(let j=1; j <= 2*i; j++)
+}
+cl
